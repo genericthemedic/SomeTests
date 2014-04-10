d51ebc54cc09c59078680b74b5fe9b17e3f4abf4
genericthemedic <atwenzel@ameritech.net>
2014-04-10 00:12:45 -0500

    This is a test CS file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeGitTests1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is some code to upload
+        }
+    }
+}
